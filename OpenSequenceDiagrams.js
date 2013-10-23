2f66d1eb7cad23a536b0282f5e7ee0aa79ab8598
tpatel <thibaut.patel@gmail.com>
2013-10-23 22:20:46 +0200

    added message to replace empty svg

--- OpenSequenceDiagrams.js
+++ OpenSequenceDiagrams.js
@@ -553,6 +553,8 @@ var Sequence = (function() {
 			}
 			height += heightParticipants;
 			svg += this.signals.getSVG(heightParticipants, width);
+			
+			if(!svg) return 'Nothing to draw yet.';
 		
 			var finalSVG =
 				'<svg  xmlns="http://www.w3.org/2000/svg" version="1.1" width="'
